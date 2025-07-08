cb17811a78d0be20d6094a6c1e70e5a782aeb134
Anderson Rodrigues <anderson.912rs@gmail.com>
2025-07-08 12:40:33 -0300

    url upgrade

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "https://83f7e371ea3a.ngrok-free.app"
+  //baseURL: "https://83f7e371ea3a.ngrok-free.app"
+  baseURL:"http://localhost:3000"
  
 })
 
